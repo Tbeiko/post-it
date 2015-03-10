b2de5bbb9aabca141ac1334c221dd6d911a4f06e
Tim Beiko <t.beiko23@gmail.com>
2015-03-10 10:47:52 +0700

    Finish Add + Edit Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,37 +1,49 @@
 class PostsController < ApplicationController
+  # Set up an instance variable for action (or redirect based on some condition)
+  before_action :set_post, only: [:show, :edit, :update]
+
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
+    @comment = Comment.new
   end
-
+  
   def new
     @post = Post.new
   end
 
   def create
-    post = Post.new(post_params)
+    @post = Post.new(post_params)
 
     if @post.save
       flash[:notice] = "Your post was created."
-      redirect_to_posts_path
-    else
-      render 'new'
+      redirect_to posts_path
+    else # Validation error
+      render :new
     end
   end
 
   def edit 
-
   end
 
   def update
-
+    if @post.update(post_params)
+      flash[:notice] = "The post was updated successfully."
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
 
   private
     def post_params
       params.require(:post).permit!
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
     end
 end
