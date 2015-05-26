5e5b92ac71c485cfaea986fc0f02fe7ac106b6d2
Tim Beiko <t.beiko23@gmail.com>
2015-05-26 11:40:26 +0800

    Add error notices to javascript

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "You've successfully signed up!"
